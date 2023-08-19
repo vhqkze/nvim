5d3b749538c6755ea653db2474afd211aad5876c
vhqkze <zqz0601@gmail.com>
2023-08-19 18:26:26 +0800

    fix(util): add system notify

--- lua/util.lua
+++ lua/util.lua
@@ -9,4 +9,20 @@ function M.get_hl(name, key)
     return vim.fn.synIDattr(vim.fn.synIDtrans(vim.api.nvim_get_hl_id_by_name(name)), key)
 end
 
+function M.system_notify(content, title, subtitle, sound)
+    if vim.fn.has("mac") == 1 then
+        local cmd = string.format('display notification "%s"', content)
+        if title ~= nil then
+            cmd = cmd .. string.format(' with title "%s"', title)
+        end
+        if subtitle ~= nil then
+            cmd = cmd .. string.format(' with subtitle "%s"', subtitle)
+        end
+        if sound ~= nil then
+            cmd = cmd .. string.format(' sound name "%s"', sound)
+        end
+        vim.system({ "osascript", "-e", cmd })
+    end
+end
+
 return M
