7784a5a591d8c3e6197707fa3a5c54520a76f60a
vhqkze <zqz0601@gmail.com>
2023-08-19 17:08:22 +0800

    fix(neorg): update config

---
+++ ftplugin/norg.lua
@@ -0,0 +1,2 @@
+vim.opt_local.conceallevel = 1
+vim.opt_local.wrap = true
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,10 +1,77 @@
 require("neorg").setup({
     load = {
         ["core.defaults"] = {},
-        ["core.norg.concealer"] = {},
-        ["core.norg.completion"] = {
+        ["core.concealer"] = {
+            config = {
+                icon_preset = "basic",
+                icons = {
+                    heading = {
+                        -- icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+                        -- icons = {"󰻃 ", "󰺕 ", "󰗝 ", "󱔛 ", "󰦤 ", "󱨧 "},
+                        icons = { "󰐾 ", "󰺕 ", "󰄻 ", "󰝨 ", "󰯊 ", "󰥺 " },
+                        --    󰝦 󰽤 󰤂     󰴲  󰞯
+                        -- 󰪥   󰐾  󰀚 󰻃 󰗮 󰋷 󰹻 󰀘 
+                        -- 󰊱 󱡝 󱡞 󰮍 󰮎 󰫤 󰫥 󰊲 󰄻 󱇯 󰘯  󰛡 󱃲 󰗣 󰲉 󰐼 󰵉 󰚯 
+                        -- 󱔛 󰡾 󱇢 󱠋 󰴈 󰠖 󰞾 󰺖 󰝨 󰮐 󰮴   󰸳 󰊖 󰸿  󰫢 󰫣 󱍿 󰈐 󰫕 󰫖
+                        --    󰓏 󰦤 󱞈 󰟞 󰒸 󰳗
+                        --   󰓒  󰓎  󱙧 󰦥 󰜀 󰯊
+                        --   󱄅 󰫉 󰫊 󰋸 󰡷 󰥺 󰖙   
+                        -- 󰜈   󱀤   󰎂
+                        --   󱨧 󰫈 󰋘  󰋙 󰛸 󰜫 󰱺 
+                        --  󰜡 󰛄 󰟼  󰖨 󰼪 󰃠
+                        --  󰽚 󱥔 󰡂 󰏄  󰂸 󰃞  󰝭 󱢖 󱪂 󰥋 󰺕 󰗝 󱔝
+                    },
+                    todo = {
+                        cancelled = {
+                            icon = "󰗨",
+                        },
+                        done = {
+                            icon = "󰄬",
+                        },
+                        on_hold = {
+                            icon = "󰏤",
+                        },
+                        pending = {
+                            icon = "",
+                        },
+                        recurring = {
+                            icon = "󱞳",
+                        },
+                        uncertain = {
+                            icon = "?",
+                        },
+                        undone = {
+                            icon = "×",
+                        },
+                        urgent = {
+                            icon = "!",
+                        },
+                    },
+                },
+            },
+        },
+        ["core.completion"] = {
             config = {
                 engine = "nvim-cmp",
+            },
+        },
+        ["core.keybinds"] = {
+            config = {
+                default_keybinds = true,
+                neorg_leader = "<Leader>",
+            },
+        },
+        ["core.journal"] = {
+            config = {
+                journal_folder = "diary",
+            },
+        },
+        ["core.dirman"] = {
+            config = {
+                use_popup = false,
+                workspaces = {
+                    default = "~/Developer/notes/neorg",
+                },
             },
         },
     },
