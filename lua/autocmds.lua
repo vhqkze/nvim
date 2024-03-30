79d422c8cc5ea0e4e4717b836bacabfc074b9696
vhqkze <zqz0601@gmail.com>
2024-03-30 14:15:51 +0800

    feat: delete buffers not used for a long time

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -38,3 +38,36 @@ vim.api.nvim_create_autocmd("BufReadPost", {
         end
     end,
 })
+
+local function close_old_bufs()
+    ---@class Buffer
+    ---@field bufnr number
+    ---@field hidden number
+    ---@field loaded number
+    ---@field lastused number
+    ---@field changed number
+
+    ---@type Buffer[]
+    local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+    table.sort(bufs, function(a, b)
+        return a.lastused < b.lastused
+    end)
+    local last_used_time = os.time() - 30 * 60 -- 30分钟前
+    local remain = #bufs
+
+    for _, buf in ipairs(bufs) do
+        if remain <= 10 then
+            break
+        end
+        local is_old = buf.lastused < last_used_time
+        local is_changed = buf.changed == 1
+        local is_invisible = buf.hidden == 1 or buf.loaded == 0
+        if is_old and is_invisible and not is_changed then
+            vim.api.nvim_buf_delete(buf.bufnr, { force = false, unload = false })
+            remain = remain - 1
+        end
+    end
+end
+---@diagnostic disable-next-line: undefined-field
+local timer = vim.uv.new_timer()
+timer:start(30 * 60 * 1000, 60 * 1000, vim.schedule_wrap(close_old_bufs))
