451fad87708ec24368f62f6b39e168c4c8100f7b
vhqkze <zqz0601@gmail.com>
2024-07-26 23:52:39 +0800

    chore: update treesitter, add `nvim-treesitter/nvim-treesitter-textobjects`

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -150,6 +150,7 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         event = "VeryLazy",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
         build = function()
             local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
             ts_update()
--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -3,36 +3,174 @@ require("nvim-treesitter.configs").setup({
         "bash",
         "comment",
         "css",
+        "csv",
+        "diff",
         "dockerfile",
+        "editorconfig",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
         "gitignore",
         "go",
         "gomod",
+        "gosum",
+        "gpp",
         "html",
         "http",
+        "ini",
         "java",
         "javascript",
+        "jq",
         "json",
         "json5",
         "jsonc",
         "lua",
         "markdown",
         "markdown_inline",
+        "nginx",
+        "nix",
         "norg",
+        "perl",
+        "pip_requirements",
         "python",
         "regex",
+        "robots",
+        "ron",
+        "rst",
+        "ruby",
+        "rust",
+        "scss",
         "sql",
+        "ssh_config",
         "swift",
+        "tmux",
         "toml",
+        "typescript",
+        "typst",
         "vim",
+        "vimdoc",
+        "vue",
+        "xml",
         "yaml",
     },
     highlight = {
         enable = true,
         sync_install = false,
         additional_vim_regex_highlighting = false,
-        disable = { "markdown" },
+        disable = function(lang, bufnr)
+            if lang == "markdown" then
+                return true
+            end
+            if vim.api.nvim_buf_line_count(bufnr) > 50000 then
+                return true
+            end
+            local max_filesize = 1024 * 1024 * 1 -- 3 MB
+            local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
     },
     indent = {
         enable = false,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            scope_incremental = "<cr>",
+            node_incremental = "+",
+            node_decremental = "-",
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                -- nvim_buf_set_keymap) which plugins like which-key display
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                -- You can also use captures from other query groups like `locals.scm`
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+                ["@parameter.outer"] = "v", -- charwise
+                ["@function.inner"] = "V", -- linewise
+                ["@function.outer"] = "V", -- linewise
+                ["@class.inner"] = "V",
+                ["@class.outer"] = "V",
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = { query = "@class.outer", desc = "Next class start" },
+                -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+                ["]o"] = "@loop.*",
+                -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+                --
+                -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+                -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+                ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+                ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+                ["]d"] = "@conditional.outer",
+            },
+            goto_previous = {
+                ["[d"] = "@conditional.outer",
+            },
+        },
     },
 })
