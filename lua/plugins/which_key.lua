3b07799baf633e8f3d1e259354bd9f15a2a29e1c
vhqkze <zqz0601@gmail.com>
2024-07-27 00:43:59 +0800

    chore: update which-key.nvim

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,11 +1,23 @@
-require("which-key").setup({
+local wk = require("which-key")
+
+wk.setup({
+    preset = "classic",
     plugins = {
         spelling = {
             enabled = true,
             suggestions = 20,
         },
     },
+    ---@param ctx { mode: string, operator: string }
+    defer = function(ctx)
+        return ctx.mode == "V" or ctx.mode == "<C-V>" or ctx.mode == "v"
+    end,
     icons = {
-        separator = "➜ ",
+        separator = " ",
     },
+})
+
+wk.add({
+    { "<leader>f", group = "file" },
+    { "<leader>h", group = "Gitsigns" },
 })
