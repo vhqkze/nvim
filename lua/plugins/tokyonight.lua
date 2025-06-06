047ef9e0382f75094934baf275099c066719326e
vhqkze <zqz0601@gmail.com>
2025-06-06 19:10:14 +0800

    feat: Move nvim-cmp highlight configuration to colorscheme
    The highlight configuration for nvim-cmp was previously set directly in
    the `nvim_cmp.lua` file. This commit moves the configuration into the
    `tokyonight.lua` colorscheme file.
    
    This change allows the nvim-cmp highlight colors to be managed by the
    colorscheme, ensuring better consistency and easier customization of the
    overall editor appearance.

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -15,5 +15,45 @@ require("tokyonight").setup({
         hl.CursorLineFold = { bg = CursorLineBg }
         hl.CursorLineSign = { bg = CursorLineBg }
         hl.BufferLineIndicatorSelected = { fg = hl.TabLineSel.fg, bg = hl.Normal.bg }
+
+        -- cmp
+        hl.CmpItemAbbrDeprecated = { fg = c.comment, bg = c.none, strikethrough = true }
+        hl.CmpItemAbbrMatch = { fg = c.blue, bg = c.none, bold = true }
+        hl.CmpItemAbbrMatchFuzzy = { fg = c.blue, bg = c.none, bold = true }
+        hl.CmpItemMenu = { fg = c.magenta, bg = c.none, italic = true }
+
+        hl.CmpItemKindField = { fg = c.dark3, bg = c.magenta }
+        hl.CmpItemKindProperty = { fg = c.dark3, bg = c.magenta }
+        hl.CmpItemKindEvent = { fg = c.dark3, bg = c.magenta }
+
+        hl.CmpItemKindText = { fg = c.dark3, bg = c.green1 }
+        hl.CmpItemKindEnum = { fg = c.dark3, bg = c.green1 }
+        hl.CmpItemKindKeyword = { fg = c.dark3, bg = c.green1 }
+        hl.CmpItemKindVariable = { fg = c.dark3, bg = c.green1 }
+
+        hl.CmpItemKindConstant = { fg = c.dark3, bg = c.yellow }
+        hl.CmpItemKindConstructor = { fg = c.dark3, bg = c.yellow }
+        hl.CmpItemKindReference = { fg = c.dark3, bg = c.yellow }
+
+        hl.CmpItemKindFunction = { fg = c.fg, bg = c.purple }
+        hl.CmpItemKindStruct = { fg = c.fg, bg = c.purple }
+        hl.CmpItemKindClass = { fg = c.fg, bg = c.purple }
+        hl.CmpItemKindModule = { fg = c.fg, bg = c.purple }
+        hl.CmpItemKindOperator = { fg = c.fg, bg = c.purple }
+
+        hl.CmpItemKindUnit = { fg = c.dark3, bg = c.orange }
+        hl.CmpItemKindSnippet = { fg = c.dark3, bg = c.orange }
+        hl.CmpItemKindFolder = { fg = c.dark3, bg = c.orange }
+        hl.CmpItemKindFile = { fg = c.dark3, bg = c.orange }
+
+        hl.CmpItemKindMethod = { fg = c.fg, bg = c.blue }
+        hl.CmpItemKindValue = { fg = c.fg, bg = c.blue }
+        hl.CmpItemKindEnumMember = { fg = c.fg, bg = c.blue }
+
+        hl.CmpItemKindInterface = { fg = c.fg, bg = c.blue1 }
+        hl.CmpItemKindColor = { fg = c.fg, bg = c.blue1 }
+        hl.CmpItemKindTypeParameter = { fg = c.fg, bg = c.blue1 }
+
+        hl.CmpItemKindCodeium = { fg = c.dark3, bg = c.cyan }
     end,
 })
